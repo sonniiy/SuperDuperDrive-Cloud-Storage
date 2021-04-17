597c1e769ae2207460505cb14306d0b032a22bb0
Sonja Goerlich <null>
2021-04-17 17:40:06 +0200

    Added redirect when user logs out

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping
-    public String loginView() {return "login";}
+    public String loginView(Model model) {
+        model.addAttribute("logout","false");
+        return "login";
+    }
 
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LogoutController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LogoutController.java
@@ -1,16 +1,22 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/logout")
 public class LogoutController {
 
-    @GetMapping
-    public String logout() {
-        return "login";
+    @PostMapping("/logout")
+    public String logout(){
+        return "redirect:/login?logout";
+    }
+    @GetMapping("/logout")
+    public String logoutView(){
+        return "redirect:/login?logout";
     }
 
 }
